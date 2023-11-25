7581aac5c9a0fb4569e75b60dd2d78bb9a2b85f1
Shakil Ahamed <shakilahamed2171@gmail.com>
2023-11-25 21:08:22 +0600

    Pagination

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Banner from '../Banner/Banner';
 import HomeDiplayProducts from '../HomeDiplayProducts/HomeDiplayProducts';
-import { useLoaderData } from 'react-router-dom';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
 const Home = () => {
-    const products = useLoaderData();
+    const [products, setProducts] = useState([]);
+    const [count, setCount] = useState(0);
+
+    const [page, setPage] = useState(0);
+    const [size, setSize] = useState(9);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/products?page=${page}&size=${size}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.count);
+                setProducts(data.products);
+                setCount(data.count);
+            })
+    }, [page, size]);
+
+    const pages = Math.ceil(count / size);
 
     return (
         <div>
@@ -15,6 +31,42 @@ const Home = () => {
                     {
                         products.map(product => <HomeDiplayProducts key={product._id} product={product}></HomeDiplayProducts>)
                     }
+                </div>
+
+                <div className='my-5 text-center flex justify-center items-center'>
+                    <button onClick={() => {
+                        if (page <= 0) {
+                            setPage(page)
+                            return;
+                        }
+                        else {
+                            setPage(page - 1)
+                        }
+                    }} className='text-xs m-4 border border-indigo-950 rounded px-2 py-[6px]'><IoIosArrowBack></IoIosArrowBack></button>
+                    {
+                        [...Array(pages).keys()].map(number => <button key={number}
+                            onClick={() => setPage(number)}
+                            className={`${page === number ? 'bg-indigo-900 text-white' : ''} m-[2px] px-2 py-1 text-xs border border-indigo-950 rounded font-bold`}>{number + 1}</button>)
+                    }
+
+                    <button onClick={() => {
+                        if ((pages - 1) === page) {
+                            setPage(page)
+                            return;
+                        }
+                        else {
+                            setPage(page + 1)
+                        }
+                    }} className='text-xs m-4 border border-indigo-950 rounded px-2 py-[6px]'><IoIosArrowForward></IoIosArrowForward></button>
+
+
+                    <select onChange={(event) => setSize(event.target.value)} className='m-4 outline-none cursor-pointer text-xs border border-indigo-950 rounded font-bold  py-1'>
+                        <option value="3">3</option>
+                        <option value="5">5</option>
+                        <option value="10" selected>9</option>
+                        <option value="15">15</option>
+                        <option value="20">20</option>
+                    </select>
                 </div>
             </div>
         </div>
--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -17,7 +17,6 @@ export const router = createBrowserRouter([
         children: [
             {
                 path: '/',
-                loader: ()=> fetch('http://localhost:5000/products'),
                 element: <Home></Home>
             },
             {
